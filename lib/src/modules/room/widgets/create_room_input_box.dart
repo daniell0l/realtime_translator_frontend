821958639ed17497b0e5513e4ddcde70fd6cae9f
daniell0l <daniel_l0l@hotmail.com>
2026-03-25 17:33:49 -0300

    melhorando o visual do input

--- lib/src/modules/room/widgets/create_room_input_box.dart
+++ lib/src/modules/room/widgets/create_room_input_box.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:realtime_translator_frontend/src/core/theme/app_colors.dart';
 
-class CreateRoomInputBox extends StatelessWidget {
+class CreateRoomInputBox extends StatefulWidget {
   final double scale;
   final Widget child;
 
@@ -11,24 +12,47 @@ class CreateRoomInputBox extends StatelessWidget {
   });
 
   @override
+  State<CreateRoomInputBox> createState() => _CreateRoomInputBoxState();
+}
+
+class _CreateRoomInputBoxState extends State<CreateRoomInputBox> {
+  bool _hasFocus = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 18 * scale),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(18 * scale),
-        border: Border.all(color: const Color(0xFFD8E0EC)),
-      ),
-      child: Theme(
-        data: Theme.of(context).copyWith(
-          inputDecorationTheme: const InputDecorationTheme(
-            hintStyle: TextStyle(
-              color: Color(0xFF8B95A7),
-              fontWeight: FontWeight.w500,
+    final borderColor = _hasFocus
+        ? AppColors.primary
+        : const Color(0xFFD8E0EC);
+    final borderWidth = _hasFocus ? 1.8 : 1.2;
+
+    return FocusScope(
+      onFocusChange: (hasFocus) {
+        if (_hasFocus == hasFocus) return;
+
+        setState(() {
+          _hasFocus = hasFocus;
+        });
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 160),
+        curve: Curves.easeOut,
+        padding: EdgeInsets.symmetric(horizontal: 18 * widget.scale),
+        decoration: BoxDecoration(
+          color: _hasFocus ? const Color(0xFFF2F5FF) : Colors.white,
+          borderRadius: BorderRadius.circular(18 * widget.scale),
+          border: Border.all(color: borderColor, width: borderWidth),
+        ),
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            inputDecorationTheme: const InputDecorationTheme(
+              hintStyle: TextStyle(
+                color: Color(0xFF8B95A7),
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ),
+          child: widget.child,
         ),
-        child: child,
       ),
     );
   }
